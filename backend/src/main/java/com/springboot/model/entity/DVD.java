214b9e9b321e7937b64f9ab50383fda7b004c3ec
hainghuu19 <nguyenhuuhai7a@gmail.com>
2024-06-14 16:30:08 +0700

    list all and detail product feature

---
+++ backend/src/main/java/com/springboot/model/entity/DVD.java
@@ -0,0 +1,35 @@
+package com.springboot.model.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+import lombok.Data;
+
+@Data
+@Entity
+public class DVD {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String discType;
+    private String director;
+    private int runtime;
+    private String studio;
+    private String language;
+    private String subtitles;
+    private String genre;
+    private Date releaseDate;
+
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "id")
+    private Product product;
+}
---
+++ backend/src/main/java/com/springboot/model/entity/LP.java
@@ -0,0 +1,32 @@
+package com.springboot.model.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+import lombok.Data;
+
+@Data
+@Entity
+public class LP {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String artist;
+    private String recordLabel;
+    private String tracklist;
+    private String genre;
+    private Date releaseDate;
+
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "id")
+    private Product product;
+}
