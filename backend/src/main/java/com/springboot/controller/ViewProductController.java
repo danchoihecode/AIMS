2c2eabb8987b5ae235507455f45a52f72a01e542
Duong Hoang Hai <hoanghai2003vp@gmail.com>
2024-06-18 11:36:56 +0700

    Resolve conflict

--- backend/src/main/java/com/springboot/controller/ViewProductController.java
+++ backend/src/main/java/com/springboot/controller/ViewProductController.java
@@ -1,20 +1,22 @@
 package com.springboot.controller;
 
-import com.springboot.model.entity.Product;
-import com.springboot.model.entity.User;
-import com.springboot.model.response.ViewProductResponse;
-import com.springboot.repository.ProductRepository;
-import com.springboot.service.ProductService;
-import com.springboot.service.ViewProductService;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.UUID;
+import com.springboot.model.entity.Product;
+import com.springboot.service.ViewProductService;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
